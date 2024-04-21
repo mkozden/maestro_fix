b6c8817cd9567dd2edc10b658ff6b5e57b62916c
godofpainTR <metekaanozden@gmail.com>
2024-04-21 16:13:45 +0300

    Added Vivado files, removed Quartus files (As they were unused)

--- Quartus/riscv_microcontroller/simulation/qsim/work/microcontroller_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity microcontroller_vlg_vec_tst is
-end microcontroller_vlg_vec_tst;
--- Quartus/riscv_microcontroller/simulation/qsim/work/riscv_microcontroller_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity riscv_microcontroller_vlg_vec_tst is
-end riscv_microcontroller_vlg_vec_tst;
